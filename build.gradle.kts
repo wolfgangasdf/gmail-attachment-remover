ecdb319b3988956dccd266a79d0c5e427d1a8536
wolfgang <wolfgang.loeffler@gmail.com>
2023-06-28 21:00:01 +0200

    aarch64

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 
 group = "com.sgar"
 version = "1.0-SNAPSHOT"
-val cPlatforms = listOf("mac", "linux", "win") // compile for these platforms. "mac", "linux", "win"
+val cPlatforms = listOf("mac-aarch64", "linux", "win") // compile for these platforms. "mac", "mac-aarch64", "linux", "win"
 val javaVersion = 19
 println("Current Java version: ${JavaVersion.current()}")
 if (JavaVersion.current().majorVersion.toInt() != javaVersion) throw GradleException("Use Java $javaVersion")
@@ -29,7 +29,7 @@ plugins {
 
 application {
     mainClass.set("sgar.Sgar")
-    applicationDefaultJvmArgs = listOf("-Dprism.verbose=true", "-Dprism.order=sw") // use software renderer
+    applicationDefaultJvmArgs = listOf("-Dprism.verbose=true")
 }
 
 repositories {
@@ -73,8 +73,9 @@ runtime {
     fun setTargetPlatform(jfxplatformname: String) {
         val platf = if (jfxplatformname == "win") "windows" else jfxplatformname // jfx expects "win" but adoptium needs "windows"
         val os = org.gradle.internal.os.OperatingSystem.current()
-        val oss = if (os.isLinux) "linux" else if (os.isWindows) "windows" else if (os.isMacOsX) "mac" else ""
+        var oss = if (os.isLinux) "linux" else if (os.isWindows) "windows" else if (os.isMacOsX) "mac" else ""
         if (oss == "") throw GradleException("unsupported os")
+        if (System.getProperty("os.arch") == "aarch64") oss += "-aarch64"// https://github.com/openjfx/javafx-gradle-plugin#4-cross-platform-projects-and-libraries
         if (oss == platf) {
             targetPlatform(jfxplatformname, javaToolchains.launcherFor(java.toolchain).get().executablePath.asFile.parentFile.parentFile.absolutePath)
         } else { // https://api.adoptium.net/q/swagger-ui/#/Binary/getBinary
@@ -106,10 +107,10 @@ open class CrossPackage : DefaultTask() {
         project.runtime.targetPlatforms.get().forEach { (t, _) ->
             println("targetplatform: $t")
             val imgdir = "${project.runtime.imageDir.get()}/${project.name}-$t"
-            println("imagedir: $imgdir")
-            when(t) {
-                "mac" -> {
-                    val appp = File(project.buildDir.path + "/crosspackage/mac/$execfilename.app").path
+            println("imagedir=$imgdir targetplatform=$t")
+            when {
+                t.startsWith("mac") -> {
+                    val appp = File(project.buildDir.path + "/crosspackage/$t/$execfilename.app").path
                     project.delete(appp)
                     project.copy {
                         into(appp)
@@ -167,9 +168,9 @@ open class CrossPackage : DefaultTask() {
                     // touch folder to update Finder
                     File(appp).setLastModified(System.currentTimeMillis())
                     // zip it
-                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-mac.zip"), File("${project.buildDir.path}/crosspackage/mac"))
+                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-$t.zip"), File("${project.buildDir.path}/crosspackage/$t"))
                 }
-                "win" -> {
+                t == "win" -> {
                     File("$imgdir/bin/$execfilename.bat").delete() // from runtime, not nice
                     val pf = File("$imgdir/$execfilename.bat")
                     pf.writeText("""
@@ -177,10 +178,10 @@ open class CrossPackage : DefaultTask() {
                         set DIR=%~dp0
                         start "" "%DIR%\bin\javaw" %JLINK_VM_OPTIONS% -classpath "%DIR%/lib/*" ${project.application.mainClass.get()} 
                     """.trimIndent())
-                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-win.zip"), File(imgdir))
+                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-$t.zip"), File(imgdir))
                 }
-                "linux" -> {
-                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-linux.zip"), File(imgdir))
+                t.startsWith("linux") -> {
+                    zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-$t.zip"), File(imgdir))
                 }
             }
         }
